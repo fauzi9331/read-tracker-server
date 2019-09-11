e3f292966fb9f44b37084e731193879adee87dde
muhammad iqbal fauzi <iqbal.fauzi9331@gmail.com>
2019-09-11 11:19:45 +0700

    simple mutation
    https://www.howtographql.com/graphql-js/3-a-simple-mutation/

--- src/index.js
+++ src/index.js
@@ -5,59 +5,60 @@
 // Implement corresponding resolver functions for the added fields
 // This process is also referred to as schema-driven or schema-first development.
 
+const { GraphQLServer } = require("graphql-yoga");
 
-const { GraphQLServer } = require('graphql-yoga')
-
-// The typeDefs constant defines your GraphQL schema (more about this in a bit). 
-// Here, it defines a simple Query type with one field called info. This field has 
-// the type String!. The exclamation mark in the type definition means that this 
-// field can never be null.
-const typeDefs = `
-type Query {
-  info: String!
-  feed: [Link]!
-}
-
-type Link{
-  id: ID!
-  description: String!
-  url: String!
-}
-`
-
-// The resolvers object is the actual implementation of the GraphQL schema. Notice how 
+// The resolvers object is the actual implementation of the GraphQL schema. Notice how
 // its structure is identical to the structure of the type definition inside typeDefs: Query.info.
 
-// The links variable is used to store the links at runtime. For now, everything is stored only 
+// The links variable is used to store the links at runtime. For now, everything is stored only
 // in-memory rather than being persisted in a database.
-let links = [{
-  id: 'link-0',
-  url: 'www.howtographql.com',
-  description: 'Fullstack tutorial for GraphQL'
-}]
+let links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL",
+  },
+];
 
+// You’re adding a new integer variable that simply serves as a way to generate unique IDs for 
+// newly created Link elements.
+let idCount = links.length;
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    // You’re adding a new resolver for the feed root field. Notice that a resolver always has to 
+    // You’re adding a new resolver for the feed root field. Notice that a resolver always has to
     // be named after the corresponding field from the schema definition.
     feed: () => links,
   },
 
-  // Finally, you’re adding three more resolvers for the fields on the Link type from the schema 
+  // Finally, you’re adding three more resolvers for the fields on the Link type from the schema
   // definition. We’ll discuss in a bit what the parent argument is that’s passed into the resolver here.
   Link: {
-    id: (parent) => parent.id,
-    description: (parent) => parent.description,
-    url: (parent) => parent.url,
-  }
-}
+    id: parent => parent.id,
+    description: parent => parent.description,
+    url: parent => parent.url,
+  },
 
-// Finally, the schema and resolvers are bundled and passed to the GraphQLServer which is imported 
-// from graphql-yoga. This tells the server what API operations are accepted and how they should be 
+  // The implementation of the post resolver first creates a new link object, then adds it to 
+  // the existing links list and finally returns the new link.
+  Mutation: {
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      };
+      links.push(link);
+      return link;
+    },
+  },
+};
+
+// Finally, the schema and resolvers are bundled and passed to the GraphQLServer which is imported
+// from graphql-yoga. This tells the server what API operations are accepted and how they should be
 // resolved.
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: ",/schema.graphql",
   resolvers,
-})
-server.start(() => console.log(`Server is running on http://localhost:4000`))
+});
+server.start(() => console.log(`Server is running on http://localhost:4000`));
